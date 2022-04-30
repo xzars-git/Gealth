c078c8160e90de2cee7aefbeb6e561e2454de057
Arsenius Purbandono <xzars@upi.edu>
2022-04-30 10:09:10 +0700

    LupaKataSandi

---
+++ app/src/main/java/com/example/myapplication/LupaPassword_EmailConfirmation.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LupaPassword_EmailConfirmation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lupapassword_emailconfirmation);
+    }
+}
